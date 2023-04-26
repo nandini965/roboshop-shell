d78e22745760ea02f36176d5c7d14e2cad3d8485
nandini965 <nandhini965@gmail.com>
2023-04-26 08:46:40 +0530

    create frontend script

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,8 @@
 yum install nginx -y
-
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-###some file is to be added
+####some file is to be added
 systemctl restart nginx
 systemctl enable nginx
