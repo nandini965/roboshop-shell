225d504bdc83203445cb479b4260f4046f1fb25f
nandini965 <nandhini965@gmail.com>
2023-04-28 22:46:29 +0530

    correcting redis file and conf files

--- redis.sh
+++ redis.sh
@@ -3,7 +3,7 @@ yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 dnf module enable redis:remi-6.2 -y
 echo -e "\e[36m>>>>>>>>>> install redis <<<<<<<<<<<\e[0n"
 yum install redis -y
-sed -i -e 's|127.0.0.1|0.0.0.o|' /etc/redis.conf /etc/redis/redis.conf
+sed -i -e 's|127.0.0.1|0.0.0.o|' etc/redis.conf etc/redis/redis.conf
 echo -e "\e[36m>>>>>>>>>>>>>> start redis service<<<<<<<<<<<\e[0n"
 systemctl enable redis
 systemctl start redis
