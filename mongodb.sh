30fd42c610bd7847e7738a6c3593e45e295f1d93
nandini965 <nandhini965@gmail.com>
2023-04-28 10:23:26 +0530

    correcting mongod file

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,7 @@
 yum install mongodb-org -y
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 systemctl enable mongod
-sed -i -e 's | 127.0.0.1 | 0.0.0.0 |* /etc/mongod.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|* /etc/mongod.conf
 systemctl restart mongod
 
 
