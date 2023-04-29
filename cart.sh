c1f17c4acb10d0dcc94b6ba18e919de8aaa3620a
nandini965 <nandhini965@gmail.com>
2023-04-29 20:26:59 +0530

    cart service

--- cart.sh
+++ cart.sh
@@ -15,7 +15,7 @@ cd /app
 echo -e "\e[36m>>>>>>>>>>>>>>install nodejs dependencies<<<<<<<<<<<<<<<<<<\e[0m"
 npm install
 echo -e "\e[36m>>>>>>>>>>>>>>copy cart client<<<<<<<<<<<<<<<<<<\e[0m"
-cp /home/centos/roboshop/cart.service /etc/systemd/system/cart.service
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service
 echo -e "\e[36m>>>>>>>>>>>>>> start cart service<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable cart
