f9f7f121bd7ffcd8aa0ad2efa72110937dcc2edb
nandini965 <nandhini965@gmail.com>
2023-04-29 22:54:54 +0530

    shipping service

--- shipping.sh
+++ shipping.sh
@@ -1,16 +1,26 @@
+echo -e "\e[36m>>>>>>>>>>>>> install maven <<<<<<<<<<<<<<\e[0m"
 yum install maven -y
+echo -e "\e[36m>>>>>>>>>>>>> add application user <<<<<<<<<<<<<<\e[0m"
 useradd roboshop
+echo -e "\e[36m>>>>>>>>>>>>> create app directory <<<<<<<<<<<<<<\e[0m"
 mkdir /app
+echo -e "\e[36m>>>>>>>>>>>>> download app content <<<<<<<<<<<<<<\e[0m"
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+echo -e "\e[36m>>>>>>>>>>>>> unzip app content <<<<<<<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/shipping.zip
 cd /app
+echo -e "\e[36m>>>>>>>>>>>>> move shipping content <<<<<<<<<<<<<<\e[0m"
 mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-cp shipping.service /etc/systemd/system/shipping.service
+mv /home/centos/roboshop-shell/target/shipping-1.0.jar shipping.jar
+echo -e "\e[36m>>>>>>>>>>>>> copy shipping.service <<<<<<<<<<<<<<\e[0m"
+cp/home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
+echo -e "\e[36m>>>>>>>>>>>>> start shipping service<<<<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable shipping
 systemctl start shipping
+echo -e "\e[36m>>>>>>>>>>>>> download my sql client <<<<<<<<<<<<<<\e[0m"
 yum install mysql -y
-mysql -h mysql.rdevopsb72.store -uroot -pRoboShop@1 < /app/schema/shipping.sql
+echo -e "\e[36m>>>>>>>>>> load schema <<<<<<<<<<<<<<\e[0m"
+mysql -h mysql-dev.rdevopsb72.store -uroot -pRoboShop@1 < /app/schema/shipping.sql
 systemctl restart shipping
