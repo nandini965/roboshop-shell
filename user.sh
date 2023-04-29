e975cf9b647e0e40d6acb3b5d76e5bbc6fa640cc
nandini965 <nandhini965@gmail.com>
2023-04-29 14:34:43 +0530

    user service

--- user.sh
+++ user.sh
@@ -1,16 +1,28 @@
+echo -e "\e[36m<<<<<<<<<<<<<<<<< configuring nodejs <<<<<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+echo -e "\e[36m<<<<<<<<<<<<<<<<< install nodejs <<<<<<<<<<<<<\e[0m"
 yum install nodejs -y
+echo -e "\e[36m<<<<<<<<<<<<<<<<< add application user <<<<<<<<<<<<<\e[0m"
 useradd roboshop
+echo -e "\e[36m<<<<<<<<<<<<<<<<< create app directory <<<<<<<<<<<<<\e[0m"
 mkdir /app
+echo -e "\e[36m<<<<<<<<<<<<<<<<< download app content <<<<<<<<<<<<<\e[0m"
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
 cd /app
+echo -e "\e[36m<<<<<<<<<<<<<<<<< unzip app content <<<<<<<<<<<<<\e[0m"
 unzip /tmp/user.zip
 cd /app
+echo -e "\e[36m<<<<<<<<<<<<<<<<< install nodejs dependencies <<<<<<<<<<<<<\e[0m"
 npm install
-cp user.service /etc/systemd/system/user.service
+echo -e "\e[36m<<<<<<<<<<<<<<<<< create application directory <<<<<<<<<<<<<\e[0m"
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
+echo -e "\e[36m<<<<<<<<<<<<<<<<< start user service <<<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable user
 systemctl start user
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[36m<<<<<<<<<<<<<<<<< copy mongodb repo <<<<<<<<<<<<<\e[0m"
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[36m<<<<<<<<<<<<<<<<< install mongodb client <<<<<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
-mongo --host mongodb.rdevopsb72.store </app/schema/user.js
+echo -e "\e[36m<<<<<<<<<<<<<<<<< load schema <<<<<<<<<<<<<\e[0m"
+mongo --host mongodb-dev.rdevopsb72.store </app/schema/user.js
