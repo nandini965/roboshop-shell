bc9524703f6f803ffd3b7aff2093e67edfdb6cb4
nandini965 <nandhini965@gmail.com>
2023-04-30 22:59:18 +0530

    03 variable.sh

--- user.sh
+++ user.sh
@@ -1,6 +1,6 @@
 source common.sh
 script_path=$(dirname $0)
-source ${script_path} /common.sh
+source${script_path} /common.sh
 exit
 echo -e "\e[36m<<<<<<<<<<<<<<<<< configuring nodejs <<<<<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
