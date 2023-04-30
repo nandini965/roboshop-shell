4e992aa501164721903fcae4e417528ba7f3f799
nandini965 <nandhini965@gmail.com>
2023-04-30 23:14:59 +0530

    script_path

--- user.sh
+++ user.sh
@@ -19,13 +19,13 @@ cd /app
 echo -e "\e[36m<<<<<<<<<<<<<<<<< install nodejs dependencies <<<<<<<<<<<<<\e[0m"
 npm install
 echo -e "\e[36m<<<<<<<<<<<<<<<<< create application directory <<<<<<<<<<<<<\e[0m"
-cp sript_path/user.service /etc/systemd/system/user.service
+cp $sript_path/user.service /etc/systemd/system/user.service
 echo -e "\e[36m<<<<<<<<<<<<<<<<< start user service <<<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable user
 systemctl start user
 echo -e "\e[36m<<<<<<<<<<<<<<<<< copy mongodb repo <<<<<<<<<<<<<\e[0m"
-cp script_path/mongo.repo /etc/yum.repos.d/mongo.repo
+cp $script_path/mongo.repo /etc/yum.repos.d/mongo.repo
 echo -e "\e[36m<<<<<<<<<<<<<<<<< install mongodb client <<<<<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
 echo -e "\e[36m<<<<<<<<<<<<<<<<< load schema <<<<<<<<<<<<<\e[0m"
