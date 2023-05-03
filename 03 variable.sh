c53dc2a6f5b0e52924d975ce3cab6657f4544c4e
nandini965 <nandhini965@gmail.com>
2023-05-03 15:50:54 +0530

    special variables for inputs

--- 03 variable.sh
+++ 03 variable.sh
@@ -3,3 +3,9 @@ name=devops
 #print variable
 echo a=$
 echo name=$name
+##special variable for inputs
+echo script name - $0
+echo first arguement -$1
+echo second arguement -$2
+echo all arguements -$*
+echo no of arguements -$#
