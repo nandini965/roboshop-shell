50ba25314411274e2dee7de5b373d5d42e99563f
nandini965 <nandhini965@gmail.com>
2023-05-05 14:41:06 +0530

    special variables for inputs

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,7 +1,7 @@
 script=$(realpath "$0")
 script_path=$(dirname $0)
-rabbitmq_appuser_password=$1
 source common.sh
+rabbitmq_appuser_password=$1
 echo -e "\e[36m>>>>>>>>>>>>> download erlang repo <<<<<<<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
 echo -e "\e[36m>>>>>>>>>>>>> install erlang -y <<<<<<<<<<<<<<\e[0m"
