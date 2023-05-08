122ea6a4fa85bcb7ba51f916bb3dfc201b7334e8
nandini965 <nandhini965@gmail.com>
2023-05-09 00:06:36 +0530

    special variables for inputs

--- catalogue.sh
+++ catalogue.sh
@@ -1,6 +1,6 @@
 script=$(realpath "$0")
 script_path=$(dirname $ "script")
-source ${script_path} common.sh
+source ${script_path}/ common.sh
 echo -e "\e[36m>>>>>>>>> configuring nodejs.repos <<<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 echo -e "\e[36m>>>>>>>>>> install nodejs <<<<<<<<<\e[0m"
--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,6 +1,6 @@
 script=$(realpath "$0")
 script_path=$(dirname $"script")
-source ${script_path} common.sh
+source ${script_path}/ common.sh
 rabbitmq_appuser_password=$1
 echo -e "\e[36m>>>>>>>>>>>>> download erlang repo <<<<<<<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
