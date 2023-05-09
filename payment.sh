27b06248d224b7e7c5b3e37266c8b3e0800a2dc9
nandini965 <nandhini965@gmail.com>
2023-05-09 08:53:40 +0530

    special variables for inputs

--- payment.sh
+++ payment.sh
@@ -1,6 +1,7 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+exit
 rabbitmq_appuser_password=$1
 echo -e "\e[36m>>>>>>>>>>>>> install python3 <<<<<<<<<<<<<<\e[0m"
 yum install python36 gcc python3-devel -y
@@ -18,7 +19,7 @@ echo -e "\e[36m>>>>>>>>>>>>> install app requirements <<<<<<<<<<<<<<\e[0m"
 pip3.6 install -r requirements.txt
 echo -e "\e[36m>>>>>>>>>>>>> copy systemd <<<<<<<<<<<<<<\e[0m"
 sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|" /home/centos/roboshop-shell/${script_path}/payment.service
-cp /home/centos/roboshop-shell/${script_path}/payment.service /etc/systemd/system/payment.service
+cp /home/centos/roboshop-shell${script_path}/payment.service /etc/systemd/system/payment.service
 echo -e "\e[36m>>>>>>>>>>>>> start payment <<<<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable payment
