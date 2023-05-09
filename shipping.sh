234bb37edc55606c3237a01b25e16638c775a83a
nandini965 <nandhini965@gmail.com>
2023-05-10 02:26:27 +0530

    source common

--- shipping.sh
+++ shipping.sh
@@ -1,5 +1,5 @@
-script_path=$(dirname "$script")
 script=$(realpath "$0")
+script_path=$(dirname "$script")
 source ${script_path}/common.sh
 mysql_root_password=$1
 echo -e "\e[36m>>>>>>>>>>>>> install maven <<<<<<<<<<<<<<\e[0m"
@@ -19,7 +19,7 @@ mvn clean package
 echo -e "\e[36m>>>>>>>>>>>>> move shipping content <<<<<<<<<<<<<<\e[0m"
 mv target/shipping-1.0.jar shipping.jar
 echo -e "\e[36m>>>>>>>>>>>>> copy systemd <<<<<<<<<<<<<<\e[0m"
-cp $script_path/shipping.service /etc/systemd/system/shipping.service
+cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
 echo -e "\e[36m>>>>>>>>>>>>> download my sql client <<<<<<<<<<<<<<\e[0m"
 yum install mysql -y
 echo -e "\e[36m>>>>>>>>>> load schema <<<<<<<<<<<<<<\e[0m"
