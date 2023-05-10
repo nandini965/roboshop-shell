03e5f887221526a4d23584561b0cd4a162e431cd
nandini965 <nandhini965@gmail.com>
2023-05-10 15:33:25 +0530

    common.sh

--- catalogue.sh
+++ catalogue.sh
@@ -5,7 +5,7 @@ component=catalogue
 func_nodejs
 
 echo -e "\e[36m<<<<<<<<<<<<<<<<< copy mongodb repo <<<<<<<<<<<<<\e[0m"
-cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+cp $script_path/mongo.repo /etc/yum.repos.d/mongo.repo
 echo -e "\e[36m<<<<<<<<<<<<<<<<< install mongodb client <<<<<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
 echo -e "\e[36m>>>>>>>>>>>>>>>> load schema <<<<<<<<<<<\e[0m"
--- common.sh
+++ common.sh
@@ -14,18 +14,15 @@ useradd ${app_user}
 echo -e "\e[36m>>>>>>>>>>>>>> create application directory<<<<<<<<<<<<<<<<<<\e[0m"
 mkdir /app
 echo -e "\e[36m>>>>>>>>>>>>>> download app content<<<<<<<<<<<<<<<<<<\e[0m"
-curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-cd /app
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip cd /app
 echo -e "\e[36m>>>>>>>>>>>>>>unzip app content<<<<<<<<<<<<<<<<<<\e[0m"
-unzip /tmp/${component}.zip
-cd /app
+unzip /tmp/${component}.zip cd /app
 echo -e "\e[36m>>>>>>>>>>>>>>install nodejs dependencies<<<<<<<<<<<<<<<<<<\e[0m"
 npm install
 echo -e "\e[36m>>>>>>>>>>>>>>copy cart client<<<<<<<<<<<<<<<<<<\e[0m"
-cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+cp $script_path/${component}.service /etc/systemd/system/${component}.service
 echo -e "\e[36m>>>>>>>>>>>>>> start cart service<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable ${component}
 systemctl restart ${component}
-
 }
