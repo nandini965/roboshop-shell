c0252d898c5bfbe87106e046b3d3437a8e53fe4e
nandini965 <nandhini965@gmail.com>
2023-05-13 00:57:31 +0530

    exit status

--- common.sh
+++ common.sh
@@ -7,12 +7,12 @@ func_print_head() {
 echo -e "\e[36m>>>>>>>>>>>>> $* <<<<<<<<<<<<<<<<<<\e[0m"
 }
 
-func_stat_check() {
+   func_stat_check() {
 if [ $1 -eq 0 ]; then
-echo -e "\e[32mSUCCESS\e[0m"
+   echo -e "\e[32mSUCCESS\e[0m"
 else
-echo -e "\e[31mFAILURE\e[0m"
-exit 1
+   echo -e "\e[31mFAILURE\e[0m"
+   exit 1
 fi
 }
  func_schema_setup() {
