134a515ce3dbb58be374b4850d403be4f8f3cc1f
nandini965 <nandhini965@gmail.com>
2023-05-12 19:41:10 +0530

    common.sh

--- common.sh
+++ common.sh
@@ -7,13 +7,13 @@ func_print_head() {
 echo -e "\e[36m>>>>>>>>>>>>> $* <<<<<<<<<<<<<<<<<<\e[0m"
 }
  func_stat_check() {
-   if [$1 -eq 0]; then
+   if [ $1 -eq 0 ]; then
    echo -e "\e [32mSUCCESS\e[0m"
    else
    echo -e "\e [31mFAILURE\e[0m"
    exit 1
    fi
-}
+ }
  func_schema_setup() {
 if [ "$schema_setup" == "mongo" ]; then
 func_print_head mongodb repos
