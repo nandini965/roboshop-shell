9f724898d388af3f37996f8bf04888bc436d91c5
nandini965 <nandhini965@gmail.com>
2023-05-15 19:56:05 +0530

    Redirectors

--- common.sh
+++ common.sh
@@ -5,6 +5,7 @@ log_file=/tmp/roboshop.log
 
 func_print_head() {
 echo -e "\e[36m>>>>>>>>>>>>> $* <<<<<<<<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>>>>> $1 <<<<<<<<<<<<<<<<<<\e[0m" &>>$log_file
 }
 
    func_stat_check() {
