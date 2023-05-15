088d770ee6f140b8b83ac6f053b7f068126157ef
nandini965 <nandhini965@gmail.com>
2023-05-15 20:51:54 +0530

    payment.sh

--- common.sh
+++ common.sh
@@ -133,8 +133,9 @@ func_python() {
   pip3.6 install -r requirements.txt &>>$log_file
   func_stat_check $?
 
-  func_print_head "update passwords in system service file
-  sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|" ${script_path}/payment.service &>>$log_file
+  func_print_head "update passwords in system service file"
+  sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|" ${script_path}/${component}.service &>>$log_file
   func_stat_check $?
-  func_systemd_setup
+
+
 }
