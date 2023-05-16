38c3ff395fa55903ddd3bcb2f880e42ef3b857eb
nandini965 <nandhini965@gmail.com>
2023-05-16 22:10:40 +0530

    rabbitmq

--- common.sh
+++ common.sh
@@ -136,6 +136,7 @@ func_python() {
   func_print_head "update passwords in system service file"
   sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|" ${script_path}/${component}.service &>>$log_file
   func_stat_check $?
+  func_schema_setup
  func_systemd_setup
 
 }
