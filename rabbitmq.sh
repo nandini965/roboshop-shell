de272f3e42d41075bd1ff99d16d9b884f4354f14
nandini965 <nandhini965@gmail.com>
2023-05-16 16:47:57 +0530

    frontend

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -8,29 +8,31 @@ echo input rabbitmq_appuser_password is missing
 exit
 fi
 
-   func_print_head  "download erlang repo"
+   func_print_head  " download erlang repo "
   curl -s https://packagecloud.io/install/repositories/component/erlang/script.rpm.sh | bash &>>$log_file
    func_stat_check $?
 
-    func_print_head "install erlang -y"
+    func_print_head " install erlang -y "
   yum install erlang -y &>>$log_file
    func_stat_check $?
 
-  func_print_head "download component repo"
+  func_print_head " download rabbitmq repo "
   curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>$log_file
    func_stat_check $?
 
-   func_print_head "install component service"
+   func_print_head " install rabbitmq service "
   yum install rabbitmq-server -y &>>$log_file
    func_stat_check $?
 
-   func_print_head "start rabbitmq service"
-  systemctl enable rabbitmq-server
-  systemctl start rabbitmq-server &>>$log_file
-   func_stat_check $?
 
   func_print_head "user add passwords"
   rabbitmqctl add_user roboshop ${rabbitmq_appuser_password} &>>$log_file
   rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$log_file
  func_stat_check $?
+
+  func_print_head " start rabbitmq service "
+   systemctl enable rabbitmq-server &>>$log_file
+   systemctl start rabbitmq-server &>>$log_file
+    func_stat_check $?
+
 
