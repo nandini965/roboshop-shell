49555b8347d141bf5cd3541f5706aee3e117b104
nandini965 <nandhini965@gmail.com>
2023-05-16 09:17:05 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -10,22 +10,19 @@ func_print_head "copy configuration file"
 cp $script_path /etc/nginx/default.d/roboshop. &>>$log_file
  func_stat_check $?
 
-func_print_head "remove old content"
+func_print_head "clean old content"
 rm -rf /usr/share/nginx/html/* &>>$log_file
  func_stat_check $?
-func_print_head "download frontned content"
+func_print_head "extract app content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
 func_stat_check $?
 
-func_print_head "create file"
-cd /usr/share/nginx/html &>>$log_file
- func_stat_check $?
-
 func_print_head " unzip frontend content"
+cd /usr/share/nginx/html &>>$log_file
 unzip /tmp/frontend.zip &>>$log_file
  func_stat_check $?
 
 func_print_head "start frontend service"
-
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
+func_stat_check $?
