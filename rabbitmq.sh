a0751e7adf02e67afb619773fbcc5c1468562f8c
nandini965 <nandhini965@gmail.com>
2023-05-22 21:01:58 +0530

    rabbitmq

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -17,7 +17,8 @@ fi
       func_stat_check $?
 
     func_print_head " install erlang & rabbitmq "
-     yum install erlang -y  rabbitmq-server -y &>>$log_file
+     yum install erlang -y &>>$log_file
+      yum install rabbitmq-server -y &>>$log_file
  func_stat_check $?
 
   func_print_head "start rabbitmq service"
