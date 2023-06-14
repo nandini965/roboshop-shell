7846fb69d32d97c9125b1fc4226a4d27543600e5
nandini965 <nandhini965@gmail.com>
2023-06-14 11:17:21 +0530

    state

--- shipping.sh
+++ shipping.sh
@@ -4,7 +4,7 @@ source ${script_path}/common.sh
 mysql_root_password=$1
 if [ -z "$mysql_root_password" ]; then
   echo input mysql_root_password is missing
-  exit 1
+  exit
 fi
 component="shipping"
 schema_setup=mysql
