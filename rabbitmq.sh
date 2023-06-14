a1fdc7fc12f0d77b6ccea4cd1c3523eb72cc29dd
nandini965 <nandhini965@gmail.com>
2023-06-14 09:59:36 +0530

    state

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -5,7 +5,7 @@ rabbitmq_appuser_password=$1
 
 if [ -z "$rabbitmq_appuser_password" ]; then
 echo input rabbitmq_appuser_password is missing
-exit
+exit 1
 fi
 
    func_print_head  " download erlang repo "
