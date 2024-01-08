9bb5391d1d3ad0a588d3201a2ea57f838297be2a
nandini965 <nandhini965@gmail.com>
2024-01-09 00:48:10 +0530

    filebeat

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -15,8 +15,8 @@ fi
      curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>$log_file
       func_stat_check $?
 
-    func_print_head " install erlang & rabbitmq "
-     yum install erlang rabbitmq-server -y &>>$log_file
+    func_print_head " install rabbitmq "
+     yum install rabbitmq-server -y &>>$log_file
  func_stat_check $?
 
   func_print_head "restart rabbitmq service"
